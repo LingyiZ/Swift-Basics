2743e7476cd81a87071a62cf4bd55fe12cde1b2a
Lingyi Zhan <shinycatherine01@gmail.com>
2020-11-07 21:58:06 -0600

    Initial Commit

---
+++ Swift Basics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Basics
+//
+//  Created by Lingyi Zhan on 11/7/20.
+//  Copyright © 2020 Lingyi Zhan. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
